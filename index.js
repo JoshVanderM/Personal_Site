7df23798b3d25cc065f0e6ab9eb72c46fd62762f
Joshua <jcv234@nau.edu>
2024-09-25 19:20:32 -0700

    Added main page
    Signed-off-by: Joshua <jcv234@nau.edu>

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-import express from express;
+import express from 'express';
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+
+// Get the current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (like CSS, JavaScript, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index");
 });
 
 // make web app run on ${port}
